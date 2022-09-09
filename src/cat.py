960f2bb226a0dad100721de45d89615535783edb
currocam <campuzanocurro@gmail.com>
2022-09-09 10:22:22 +0200

    refactor: add naming

--- src/cat.py
+++ src/cat.py
@@ -7,8 +7,11 @@ flags, args = split_args(sys.argv[1:])
 if not args:
     args.append('/dev/stdin')
 
-for line_number, line in enumerate(itertools.chain.from_iterable(map(open, args))):
+files_opener = map(open, args)
+for line_number, line in enumerate(itertools.chain.from_iterable(files_opener)):
     if '-n' in flags:
         print(f"{line_number}\t{line}", sep='')
     else:
         print(line, sep='')
+
+

ad6a78717b59ef9fbbe66da7f29d957bbdd6ac92
currocam <campuzanocurro@gmail.com>
2022-09-09 09:47:39 +0200

    feat: echo

--- src/echo.py
+++ src/echo.py
@@ -1,3 +1,34 @@
+from ast import arg
 import sys
 
-print(' '.join(sys.argv))
+
+def split_args(args: list[str]) -> tuple[list[str], list[str]]:
+    """
+    Split argv and return the flags separated from the rest.
+
+    >>> split_args(['-n', 'foo', 'bar'])
+    (['-n'], ['foo', 'bar'])
+    >>> split_args(['-n', 'foo', '-s', 'bar'])
+    (['-n', '-s'], ['foo', 'bar'])
+    """
+    flags, rest = [], []
+    for arg in args:
+        if arg.startswith('-'):
+            flags.append(arg)
+        else:
+            rest.append(arg)
+    return flags, rest
+
+
+flags, args = split_args(sys.argv[1:])
+if '-n' in flags:
+    end = ""
+else:
+    end = "\n"
+
+if '-n' in flags:
+    sep = ""
+else:
+    sep = " "
+
+print(sep.join(args), end=end)

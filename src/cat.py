dc5d14bacc35434a043ff173fcb296d0999d925b
currocam <campuzanocurro@gmail.com>
2022-09-09 10:12:58 +0200

    feat: cat -n

--- src/cat.py
+++ src/cat.py
@@ -1,4 +1,15 @@
 import sys
+from echo import split_args
 
-for line in sys.stdin:
-    print(line, end = "")
+flags, args = split_args(sys.argv[1:])
+
+if not args:
+    args.append('/dev/stdin')
+
+for arg in args:
+    with open(arg) as f:
+        for line_number, line in enumerate(f):
+            if '-n' in flags:
+                print(f"{line_number}\t{line}", sep='')
+            else:
+                print(line, sep='')
